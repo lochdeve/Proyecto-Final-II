bd6f4a2ce758495fa8ecb776ca0589235e8aa2b0
Carlos Garcia <alu0101208268.ull.edu.es>
2022-01-26 17:34:57 +0000

    adding sounds, effects and fixing some bugs

--- SimonDice/Assets/Scripts/animationFigureController.cs
+++ SimonDice/Assets/Scripts/animationFigureController.cs
@@ -9,8 +9,8 @@ using CameraFading;
 public class animationFigureController : MonoBehaviour
 {
     Animator animator, animator2, animator3;
-    AudioSource sniffSource;
-    public AudioClip sniffAudio;
+    AudioSource audioSource;
+    public AudioClip roarAudio,hardRoarAudio,winAudio;
     EventTrigger characterEvent, characterEvent2, characterEvent3;
     public GameObject[] figures;
     public TextMesh levelText, winLoseText;
@@ -20,7 +20,10 @@ public class animationFigureController : MonoBehaviour
     int[] order = new int[10];
     int indiceOrder = 0;
     int lastOne = 0;
-    int lastLevel = 11;
+    int lastLevel = 6;
+    bool loser = false;
+    bool playerTurn = false;
+    bool exit = false;
 
     public delegate void changeSprite();
     public static event changeSprite lose;
@@ -28,8 +31,8 @@ public class animationFigureController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        sniffSource = GetComponent<AudioSource>();
-        sniffSource.clip = sniffAudio;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = roarAudio;
         animator = figures[0].GetComponent<Animator>();
         characterEvent = figures[0].GetComponent<EventTrigger>();
         animator2 = figures[1].GetComponent<Animator>();
@@ -40,7 +43,6 @@ public class animationFigureController : MonoBehaviour
         animators[0] = animator;
         animators[1] = animator2;
         animators[2] = animator3;
-        //execute = true;
         
         characterEvent.triggers.Add(SetEvent(0,animator));
         characterEvent2.triggers.Add(SetEvent(1,animator2));
@@ -61,69 +63,95 @@ public class animationFigureController : MonoBehaviour
         indiceOrder = 0;
         execute = false;
         levelText.text ="";
+        exit = true;
     }
 
     EventTrigger.Entry SetEvent( int figura,Animator animator){
         EventTrigger.Entry entry = new EventTrigger.Entry();
         entry.eventID = EventTriggerType.PointerDown;
         entry.callback.AddListener((data) => { 
-            if(order[indiceOrder] == figura && (indiceOrder == (level-1))){
-                indiceOrder =0;
-                animator.SetBool("IdleSnuff",true);
-                sniffSource.Play(0);
-                execute = true;
-                winLoseText.text = "That was correct, Next level...";
-                //animator.SetBool("IdleSnuff",false);
-                level++;
-            } else  if (order[indiceOrder] == figura){
-             animator.SetBool("IdleSnuff",true);
-             sniffSource.Play(0);
-             indiceOrder++;
-            } else{
-                levelText.text ="Level: " + level.ToString();
-                winLoseText.text = "You lose";
-                lose();
-                CameraFade.Out(() =>
-                {
-                    winLoseText.text = "";
-                    levelText.text ="";
-                    CameraFade.In(2f);
-                }, 2f);
+            if (playerTurn)
+            {
+                if(order[indiceOrder] == figura && (indiceOrder == (level-1))){
+                
+                    indiceOrder =0;
+                    animator.SetBool("Roar",true);
+                    audioSource.Play(0);
+
+                    execute = true;
+                    winLoseText.text = "That was correct, Next level...";
+                    level++;
+                    playerTurn = false;
+                } else  if (order[indiceOrder] == figura){
+                animator.SetBool("Roar",true);
+                audioSource.Play(0);
+                
+                indiceOrder++;
+                } else{
+                    levelText.text ="Level: " + level.ToString();
+                    winLoseText.text = "You lose";
+                    lose();
+                    loser= true;
+                    CameraFade.Out(() =>
+                    {
+                        winLoseText.text = "";
+                        levelText.text ="";
+                        CameraFade.In(2f);
+                    }, 2f);
+                    playerTurn = false;
+                    exit = true;
+                }
             }
          });
         return entry;
     }
 
      IEnumerator sleepLoop() {
-          float waitTime = 2;
+          float waitTime = 3;
+           audioSource.clip = roarAudio;
             execute = false;
+            exit = false;
             levelText.text ="Level: " + level.ToString();
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(waitTime);
             winLoseText.text = "Start level";
 
             if (level == lastLevel)
             {
-                winLoseText.text = "You pass all the levels";
+                winLoseText.text = "You Win";
+                audioSource.clip = winAudio;
+                audioSource.Play(0);
+
                 lose();
             }else {
-                for (int i = 0; i < level; i++){
+                for (int i = 0; i < level && !exit; i++){
                     Random r = new Random();
                     int rInt = Random.Range(0, 3); 
                     Debug.Log("Execution figure number: " +rInt);
                     order[i] = rInt;
-                    animators[rInt].SetBool("IdleSnuff",true);
-                    sniffSource.Play(0);
-                    yield return new WaitForSeconds(waitTime);
+                    animators[rInt].SetBool("Roar",true);
+                    audioSource.Play(0);
+                    yield return new WaitForSeconds(2.5f);
                     winLoseText.text = "";
                 }
+                playerTurn = true;
             }
      }
+
     // Update is called once per frame
     void Update()
     {
         if (execute)
         {
            StartCoroutine("sleepLoop");
+        }
+        if (loser)
+        {
+            animators[0].SetBool("Roar",true);
+            animators[1].SetBool("Roar",true);
+            animators[2].SetBool("Roar",true);
+            audioSource.clip = hardRoarAudio;
+            audioSource.Play(0);
+            loser = false;
         }
     }
 }
