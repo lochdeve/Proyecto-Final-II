433fc3db9f4e7153a97b2c119b0dad60f3ddb1ec
Carlos Garcia <alu0101208268.ull.edu.es>
2022-01-27 23:35:37 +0000

    adding compass

---
+++ Simonsters/Assets/Scripts/startButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class startButton : MonoBehaviour
+{
+    EventTrigger characterEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        characterEvent = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerDown;
+        entry.callback.AddListener((data) =>
+        {
+            SceneManager.LoadScene("Game");
+        });
+        characterEvent.triggers.Add(entry);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TaskOnClick()
+    {
+    }
+}
