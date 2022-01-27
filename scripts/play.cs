4e56405e17c0c0d8dc76174f7493eed7d66e38a0
Eduardo Expósito <72284880+EduardoEB3@users.noreply.github.com>
2022-01-27 12:58:38 +0000

    Finishing scripts

--- scripts/play.cs
+++ scripts/play.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,11 +6,13 @@ using UnityEngine.EventSystems;
 
 public class play : MonoBehaviour
 {
-    public GameObject button, camara, muro;
-    EventTrigger buttonEvent;
-
+    public GameObject button, camera, wall;
+    
     public SpriteRenderer spriteRenderer;
+    
     public Sprite finishSprite, playSprite, dissappearSprite;
+    
+    EventTrigger buttonEvent;
 
     int spriteStatus = 0;
 
@@ -21,26 +23,29 @@ public class play : MonoBehaviour
     void Start()
     {
         buttonEvent = button.GetComponent<EventTrigger>();
-
         EventTrigger.Entry entry = new EventTrigger.Entry();
         entry.eventID = EventTriggerType.PointerDown;
-        entry.callback.AddListener((data) => { 
-           if (spriteStatus ==0)
-           {
+        entry.callback.AddListener((data) =>
+        {
+            if (spriteStatus == 0)
+            {
                 spriteRenderer.sprite = finishSprite;
                 start();
                 spriteStatus = 1;
-           }else  if (spriteStatus == 1){
+            }
+            else if (spriteStatus == 1)
+            {
                 spriteRenderer.sprite = playSprite;
                 finish();
                 spriteStatus = 0;
-           }
-         });
+            }
+        });
         buttonEvent.triggers.Add(entry);
         animationFigureController.lose += lose;
     }
 
-    void lose() {
+    void lose()
+    {
         spriteRenderer.sprite = playSprite;
         spriteStatus = 0;
         finish();
@@ -49,14 +54,17 @@ public class play : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float distanciaJugador = Vector3.Distance(camara.transform.position, muro.transform.localPosition);
-        if (distanciaJugador > 9) {
+        float distanciaJugador = Vector3.Distance(camera.transform.position, wall.transform.localPosition);
+        if (distanciaJugador > 9)
+        {
             spriteRenderer.sprite = dissappearSprite;
             finish();
             spriteStatus = 2;
-        }else if(spriteStatus == 2) {
+        }
+        else if (spriteStatus == 2)
+        {
             spriteStatus = 0;
             spriteRenderer.sprite = playSprite;
-        } 
+        }
     }
 }
